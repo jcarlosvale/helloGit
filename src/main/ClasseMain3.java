cfa4cd87eafbea6df738e65ec520feaea75ba932
Joao Vale <jcarlos.vale@gmail.com>
2023-05-29 23:21:47 +0100

    uso stash

--- src/main/ClasseMain3.java
+++ src/main/ClasseMain3.java
@@ -1,4 +1,7 @@
 package main;
 
 public class ClasseMain3 {
+    /*
+    algum texto
+     */
 }
--- src/main/Hello.java
+++ src/main/Hello.java
@@ -2,6 +2,6 @@ package main;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Mensagem BR");
+        System.out.println("Ola Mundo");
     }
 }
