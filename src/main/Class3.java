deceea626d0888b237ad81a7a50e7dd0286ea0dd
Joao Vale <jcarlos.vale@gmail.com>
2023-05-29 23:11:40 +0100

    feature 1

--- src/main/Class3.java
+++ src/main/Class3.java
@@ -1,4 +1,11 @@
 package main;
 
 public class Class3 {
+    public static void main(String[] args) {
+        System.out.println("Teste");
+        outroMetodo();
+    }
+
+    private static void outroMetodo() {
+    }
 }
---
+++ src/main/Class3v2.java
@@ -0,0 +1,7 @@
+package main;
+
+public class Class3v2 {
+    public static void main(String[] args) {
+        System.out.println("Outro texto");
+    }
+}
